63d854334973a3ec94356f8994f74033dd9274a4
kyo100 <rkdrydms12@naver.com>
2019-12-18 09:40:02 +0900

    ver01

---
+++ src/main/java/com/example/persistence/MemberDAOImpl.java
@@ -0,0 +1,31 @@
+package com.example.persistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.MemberVO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO{
+	@Inject
+	private SqlSession session; //데이터베이스 Connection 객체
+	private static final String namespace="memberMapper";
+	
+	@Override
+	public List<MemberVO> list() {
+		// TODO Auto-generated method stub
+		return session.selectList(namespace + ".list");
+	}
+
+	@Override
+	public void insert(MemberVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
